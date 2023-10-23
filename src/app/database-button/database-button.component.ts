0993504ebef27a920738cb22d182115538eba839
tish001 <tahminatish001@gmail.com>
2023-10-23 12:11:08 +0600

    database button mat-raised-button & arrow svg

--- src/app/database-button/database-button.component.ts
+++ src/app/database-button/database-button.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-database-button',
   templateUrl: './database-button.component.html',
-  styleUrls: ['./database-button.component.scss']
+  styleUrls: ['./database-button.component.scss'],
 })
 export class DatabaseButtonComponent {
-
+  constructor(private router: Router){}
+  onBack()
+  {
+   this.router.navigate(['/']);
+  }
 }
